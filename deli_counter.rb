5c627439ecd518f6d041a7e7e17c8f9c148ad66a
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 10:50:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,11 +5,15 @@ def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
     elsif katz_deli.length > 0 
+      line_with_names = []
       counter = 1 
       katz_deli.each do |name|
-      puts "The line is currently: #{counter}. #{name}"
-      counter += 1 
+        position_and_name = "#{counter}. #{name}"
+        counter += 1 
+        return line_with_names
     end 
+    line_with_names.each do |position_and_name|
+    puts "The line is currently: #{position_and_name}."
   end
 end 
   
@@ -24,6 +28,6 @@ def now_serving(katz_deli)
     puts "Currently serving #{name}."
     katz_deli.pop
     end
-end 
+  end 
 end 
   
