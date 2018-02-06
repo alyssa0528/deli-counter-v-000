a0f72afa3a722fa2d79613cf92877ac3569cce78
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 10:45:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def now_serving(katz_deli)
   if katz_deli.length == 0 
     puts "There is nobody waiting to be served!"
   elsif katz_deli.length > 0 
-  katz_deli.each do |name| 
+    katz_deli.each do |name| 
     puts "Currently serving #{name}."
     katz_deli.pop
   end
