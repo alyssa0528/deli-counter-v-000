eaf1e386bb58c23acf5e5c8898d7b6cb4596174f
Alyssa Kim <alyssa.kim@gmail.com>
2018-02-06 10:39:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli, index)
+  if katz_deli.length == 0 
+    "The line is currently empty."
+    elsif katz_deli.length > 0 
+      counter = 1 
+      katz_deli.each do |name|
+      "The line is currently: #{counter}. #{name}"
+      counter += 1 
+  
+  
